3b9c3b31a06fcdea3abf8cdb7854b8ad154d3b3e
Allagadda Manoj <allagaddamanoj812@gmail.com>
2023-11-17 18:53:38 +0530

    add

---
+++ src/components/topbar/Topbar.jsx
@@ -0,0 +1,42 @@
+import "./topbar.css";
+import SearchIcon from '@mui/icons-material/Search';
+
+export default function Topbar() {
+  return (
+    <div className="topbarContainer">
+      <div className="topbarLeft">
+        <span className="logo">Lamasocial</span>
+      </div>
+      <div className="topbarCenter">
+        <div className="searchbar">
+          <SearchIcon  className="searchIcon" />
+          <input
+            placeholder="Search for friend, post or video"
+            className="searchInput"
+          />
+        </div>
+      </div>
+      <div className="topbarRight">
+        <div className="topbarLinks">
+          <span className="topbarLink">Homepage</span>
+          <span className="topbarLink">Timeline</span>
+        </div>
+        <div className="topbarIcons">
+          <div className="topbarIconItem">
+            <SearchIcon  />
+            <span className="topbarIconBadge">1</span>
+          </div>
+          <div className="topbarIconItem">
+            <SearchIcon  />
+            <span className="topbarIconBadge">2</span>
+          </div>
+          <div className="topbarIconItem">
+            <SearchIcon  />
+            <span className="topbarIconBadge">1</span>
+          </div>
+        </div>
+        <img src="/assets/person/1.jpeg" alt="" className="topbarImg"/>
+      </div>
+    </div>
+  );
+}
